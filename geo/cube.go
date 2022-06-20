2726da4838e54b59b07c8ee994aaf501827b27e0
Dhanaraj <dhanaraj.gangulli@gmail.com>
2022-06-20 14:22:35 +0530

    updates

--- geo/cube.go
+++ geo/cube.go
@@ -5,7 +5,7 @@ import "errors"
 func CubeVolume(n int) (int, error) {
 
 	if n == 0 {
-		return 0, errors.New("Zero cant be")
+		return 0, errors.New("zero cant be")
 	} else {
 		return n * n * n, nil
 	}
